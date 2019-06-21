b707abc9925bfbf551218cd88038fddcaf8bc30f
Kyle Stewart <4b796c65@gmail.com>
2019-06-21 14:34:46 -0700

    Add hard-coded rooms.

--- gamemap.py
+++ gamemap.py
@@ -1,9 +1,45 @@
-from typing import List
+from typing import List, Tuple
 
 import numpy as np  # type: ignore
 import tcod.console
 
 import entity
+
+
+class Room:
+    def __init__(self, x: int, y: int, width: int, height: int):
+        self.x1 = x
+        self.y1 = y
+        self.x2 = x + width
+        self.y2 = y + height
+
+    @property
+    def outer(self) -> Tuple[slice, slice]:
+        """Return the NumPy index for the whole room."""
+        index: Tuple[slice, slice] = np.s_[self.x1 : self.x2, self.y1 : self.y2]
+        return index
+
+    @property
+    def inner(self) -> Tuple[slice, slice]:
+        """Return the NumPy index for the inner room area."""
+        index: Tuple[slice, slice] = np.s_[
+            self.x1 + 1 : self.x2 - 1, self.y1 + 1 : self.y2 - 1
+        ]
+        return index
+
+    @property
+    def center(self) -> Tuple[int, int]:
+        """Return the index for the rooms center coordinate."""
+        return (self.x1 + self.x2) // 2, (self.y1 + self.y2) // 2
+
+    def intersects(self, other: "Room") -> bool:
+        """Return True if this room intersects with another."""
+        return (
+            self.x1 <= other.x2
+            and self.x2 >= other.x1
+            and self.y1 <= other.y2
+            and self.y2 >= other.y1
+        )
 
 
 class GameMap:
@@ -21,11 +57,18 @@ class GameMap:
         self.tiles = np.empty(self.shape, dtype=self.DTYPE, order="F")
         self.tiles[...] = (1, 1)
         self.entities: List[entity.Entity] = []
+        self.rooms: List[Room] = []
 
     def generate(self) -> None:
-        self.tiles[...] = (1, 1)
+        self.tiles[...] = (0, 0)
+        self.rooms = []
 
-        self.tiles[30:33, 22] = (0, 0)
+        room1 = Room(20, 15, 10, 15)
+        room2 = Room(35, 15, 10, 15)
+
+        self.tiles[room1.inner] = (1, 1)
+        self.tiles[room2.inner] = (1, 1)
+        self.tiles[tcod.line_where(*room1.center, *room2.center)] = (1, 1)
 
         self.player = entity.Entity(
             self.width // 2, self.height // 2, ord("@"), (255, 255, 255)
